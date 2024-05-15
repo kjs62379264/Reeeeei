4dc195f605fe2d9666cf9eaaad948ba07bd15443
Junseok Kwon <sr842697@naver.com>
2024-05-16 01:23:20 +0900

    front-web3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route} from 
-'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+
 function App() {
   return (
     <>
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact />
+        <Route path='/' exact Component={Home} />
       </Routes>
     </Router>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css';
 import { Button } from './Button';
