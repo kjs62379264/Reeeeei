2fe74d0a4d8d94cb1e98787237d83e0880090945
Junseok Kwon <sr842697@naver.com>
2024-05-20 23:35:40 +0900

    front-web 7(back..)

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
-import Card from '../Card';
+
 
 function Home () {
     return (
         <>
           <HeroSection />
-          <Card/>
         </>
     );
 }
