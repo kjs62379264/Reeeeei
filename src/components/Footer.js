e7be39fe90c5e7e5222a367b345dbce4fb46be45
Junseok Kwon <sr842697@naver.com>
2024-05-22 18:43:53 +0900

    front-web 8 (footer in)

---
+++ src/components/Footer.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Button } from './Button'
+import './Footer.css'
+import { Link } from 'react-router-dom'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <section className="footer-subscription"> 
+            <p className="footer-subscription-heading">
+                우리강산 푸르게 푸르게 명지 파이팅
+            </p>
+            <p className="footer-subscription-text">
+                이이이잉앗살라말라이쿰
+            </p>
+            <div className="input-areas">
+                <form>
+                    <input type="email" name="email" placeholder="Your Email" 
+                    className="footer-input"/>
+                    <Button buttonStyle='btn--outline'>구독</Button>
+                </form>
+            </div>
+        </section>
+        <div className='footer-links'>
+            <div className="footer-link-wrapper">
+                <div className="footer-links-items">
+                    <h2>About Us</h2>
+                    <Link to='/sign-up'>How it works</Link>
+                    <Link to='/'>증명서</Link>
+                    <Link to='/'>Careers</Link>
+                    <Link to='/'>만든사람들</Link>
+                    <Link to='/'>서비스약관</Link>
+                </div>
+                <div className="footer-links-items">
+                    <h2>Contact Us</h2>
+                    <Link to='/'>연락처</Link>
+                    <Link to='/'>지원</Link>
+                    <Link to='/'>목적지</Link>
+                    <Link to='/'>후원</Link>
+                </div>
+            </div>
+            <div className="footer-link-wrapper">
+                <div className="footer-links-items">
+                    <h2>소셜미디어</h2>
+                    <Link to='/'>인스타그램</Link>
+                    <Link to='/'>유튜브</Link>
+                    <Link to='/'>페이스북</Link>
+                </div>
+            </div>
+        </div>
+        <section className="social-media">
+            <div className="social-media-wrap">
+                <div className="footer-logo">
+                    <Link 새='/' className="social-logo">
+                        여행길<i className="fab fa-typo3"></i>
+                    </Link>
+                </div>
+                <small className="website-rights">캡스톤 2024</small>
+                <div className="social-icons">
+                    <Link className="social-icon-link facebook"
+                          to='/'
+                          target='_blank'
+                          aria-label='Facebook'
+                    >
+                        <i className="fab fa-facebook-f"></i>
+                    </Link>
+                    <Link className="social-icon-link instagram"
+                          to='/'
+                          target='_blank'
+                          aria-label='Instagram'
+                    >
+                        <i className="fab fa-instagram"></i>
+                    </Link>
+                    <Link className="social-icon-link youtube"
+                          to='/'
+                          target='_blank'
+                          aria-label='Youtube'
+                    >
+                        <i className="fab fa-youtube"></i>
+                    </Link>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src='/videos/video-2.mp4' autoPlay loop muted />
+        <video src='/videos/video-1.mp4' autoPlay loop muted />
         <h1>여행 길찾기</h1>
         <p>편한 여행길 도와드립니다</p>
         <div className='hero-btns'>
